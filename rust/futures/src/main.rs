89cadf894e1d3e729f84308a0fc93985cb9d4208
alexeyz041 <alexeyz041@gmail.com>
2018-11-01 22:43:53 +0200

    run fmt and clippy

--- rust/futures/src/main.rs
+++ rust/futures/src/main.rs
@@ -1,13 +1,12 @@
 use std::io::{Error, ErrorKind};
 
 extern crate futures;
-use futures::prelude::*;
-use futures::future::ok;
 use futures::future::err;
+use futures::future::ok;
+use futures::prelude::*;
 
 extern crate tokio_core;
 use tokio_core::reactor::Core;
-
 
 fn my_fn() -> Result<u32, Box<Error>> {
     Ok(100)
@@ -18,7 +17,6 @@ fn my_fut() -> impl Future<Item = u32, Error = Box<Error>> {
     //err(Box::new(Error::new(ErrorKind::Other,"bad luck")))
 }
 
-
 fn my_fn_squared(i: u32) -> Result<u32, Box<Error>> {
     Ok(i * i)
 }
@@ -27,14 +25,16 @@ fn my_fut_squared(i: u32) -> impl Future<Item = u32, Error = Box<Error>> {
     ok(i * i)
 }
 
-
-fn main()
-{
+fn main() {
     let mut reactor = Core::new().unwrap();
     let retval = reactor.run(my_fut()).unwrap();
-    println!("{:?} {:?}", my_fn().unwrap(),retval);
+    println!("{:?} {:?}", my_fn().unwrap(), retval);
 
     let chained_future = my_fut().and_then(|retval| my_fut_squared(retval));
     let retval2 = reactor.run(chained_future).unwrap();
-    println!("{:?} {:?}", my_fn().and_then(|r|my_fn_squared(r)).unwrap(),retval2);
+    println!(
+        "{:?} {:?}",
+        my_fn().and_then(|r| my_fn_squared(r)).unwrap(),
+        retval2
+    );
 }
