0f8eb48cd551ddd432f4fa6d01344cdae572fdd1
alexeyz041 <alexeyz041@gmail.com>
2019-02-16 12:11:39 +0200

    update mmap example

--- rust/shm/src/main.rs
+++ rust/shm/src/main.rs
@@ -4,27 +4,37 @@ extern crate memmap;
 use std::env;
 use std::fs::File;
 use std::fs::OpenOptions;
-use memmap::Mmap;
+//use memmap::Mmap;
 use memmap::MmapMut;
+use std::io::Write;
 
 fn main() {
     let op = env::args()
         .nth(1)
-	.expect("supply operation r or w");
+	.expect("supply operation r,w or c");
     if op == "r" {
 	read();
     } else if op == "w" {
 	write();
+    } else if op == "c" {
+	create();
     } else {
 	println!("?{}",op);
     }
 }
 
 fn read() {
-    let file = File::open("/dev/shm/malex.hello03.01").expect("failed to open the file");
-    let mmap = unsafe { Mmap::map(&file).expect("failed to map the file") };
+//    let file = File::open("/dev/shm/malex.hello03.01").expect("failed to open the file");
+//    let mmap = unsafe { Mmap::map(&file).expect("failed to map the file") };
+
+    let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .open("/dev/shm/malex.hello03.01")
+            .expect("failed to open the file");
+    let mmap = unsafe { MmapMut::map_mut(&file).expect("failed to map the file") };
     for i in 0..10 {
-    	println!("{}",mmap[i]);
+    	println!("{:3} {}",mmap[i], mmap[i] as char);
     }
 }
 
@@ -37,5 +47,11 @@ fn write() {
     let mut mmap = unsafe { MmapMut::map_mut(&file).expect("failed to map the file") };
     mmap[0..10].copy_from_slice(b"0123456789");
 }
+
+fn create() {
+    let mut file = File::create("/dev/shm/malex.hello03.01").unwrap();
+    file.write_all(b"Hello, world!").unwrap();
+}
+
 
 
