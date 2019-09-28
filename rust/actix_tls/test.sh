0a34de73395bb2157e82ec5ca8326e784309523f
alexeyz041 <alexeyz041@gmail.com>
2019-09-28 08:53:38 +0300

    Update reqwest tls example

--- rust/actix_tls/test.sh
+++ rust/actix_tls/test.sh
@@ -6,6 +6,12 @@ cat 123
 
 curl -X POST --data-binary 'Hello' https://127.0.0.1:8443/post --cacert ca.pem
 
+rm -f 123
+wget --ca-certificate=ca.pem https://localhost:8443/get/123
+cat 123
+
+curl -X POST --data-binary 'Hello' https://localhost:8443/post --cacert ca.pem
+
 
 
 
--- rust/reqwest_tls/src/main.rs
+++ rust/reqwest_tls/src/main.rs
@@ -8,24 +8,26 @@ fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "request=debug");
     env_logger::init();
 
+    // sed -i -e 's/TRUSTED //g' ca.pem
     let mut buf = Vec::new();
-//    File::open("ca.pem").unwrap().read_to_end(&mut buf).unwrap();
-//    let cert = reqwest::Certificate::from_pem(&buf).unwrap();
-    File::open("ca.der").unwrap().read_to_end(&mut buf).unwrap();
-    let cert = reqwest::Certificate::from_der(&buf).unwrap();
+    File::open("ca.pem").unwrap().read_to_end(&mut buf).unwrap();
+    let cert = reqwest::Certificate::from_pem(&buf).unwrap();
+
+//    File::open("ca.der").unwrap().read_to_end(&mut buf).unwrap();
+//    let cert = reqwest::Certificate::from_der(&buf).unwrap();
 
     let client = reqwest::Client::builder()
 		    .use_rustls_tls()
-//		    .use_default_tls()
 		    .add_root_certificate(cert)
-		    .danger_accept_invalid_hostnames(true)
-		    .danger_accept_invalid_certs(true)
 		    .build().unwrap();
 
-    let body = client.get("https://127.0.0.1:8443/get/123").send().unwrap()
+    let body = client.get("https://localhost:8443/get/123").send().unwrap()
 	.text().unwrap();
+    info!("get body = {:?}", body);
 
-    info!("body = {:?}", body);
+    let body = client.post("https://localhost:8443/post").body("Hello from reqwest").send().unwrap()
+	.text().unwrap();
+    info!("post body = {:?}", body);
 
     Ok(())
 }
