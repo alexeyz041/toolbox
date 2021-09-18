52c5f8116261e2738c147d20b19ab4b315f46718
alexeyz041 <alexeyz041@gmail.com>
2021-09-18 08:29:15 +0300

    Update cairo version

--- rust/csv_tools/calc/src/main.rs
+++ rust/csv_tools/calc/src/main.rs
@@ -27,9 +27,9 @@ fn convert_c(c: u16) -> f64 {
 
 fn load(fnm: &str, n: usize) -> Vec<Data<f64>>
 {
-	println!("Loading {}",fnm);
-	let mut data = Vec::new();
-	let f = File::open(fnm).unwrap();
+    println!("Loading {}",fnm);
+    let mut data = Vec::new();
+    let f = File::open(fnm).unwrap();
     let file = BufReader::new(&f);
     for line in file.lines() {
     	if n != 0 && data.len() >= n {
@@ -41,18 +41,18 @@ fn load(fnm: &str, n: usize) -> Vec<Data<f64>>
     	let time = &w[0];
     	let current = &w[1];
     	
-		let t = time.parse::<u16>().unwrap();
-		let c = current.parse::<u16>().unwrap();
+	let t = time.parse::<u16>().unwrap();
+	let c = current.parse::<u16>().unwrap();
       	data.push(Data{ current: convert_c(c), time: convert_t(t) });
     }
-	data
+    data
 }
 
 fn integ(data: &[Data<f64>], start_t: usize, end_t: usize) -> f64 {
 	let mut ig = 0.0;
 	let dt = data[1].time - data[0].time;
 	for i in start_t..end_t+1 {
-		let mut val0 = if i > 0 { data[i-1].current } else { data[0].current };
+		let val0 = if i > 0 { data[i-1].current } else { data[0].current };
 		let val1 = data[i].current;
 		ig += (val1+val0)*dt/2.;
 	}
@@ -62,29 +62,29 @@ fn integ(data: &[Data<f64>], start_t: usize, end_t: usize) -> f64 {
 
 fn main()
 {   
-	let fnm = env::args().skip(1).next().expect("Missing input file");
- 	let data = load(&fnm, 200);
+    let fnm = env::args().skip(1).next().expect("Missing input file");
+    let data = load(&fnm, 200);
 
-	let (maxc, maxt) = data.iter().map(|d| (OrderedFloat(d.current), d.time))
+    let (maxc, maxt) = data.iter().map(|d| (OrderedFloat(d.current), d.time))
 		.max_by(|(x,_),(y,_)| x.cmp(y)).unwrap();
-	let maxc = maxc.into_inner();
-	let maxp = data.iter().position(|d| d.time == maxt).unwrap();
+    let maxc = maxc.into_inner();
+    let maxp = data.iter().position(|d| d.time == maxt).unwrap();
 		
-	let (minc, mint) = data.iter().map(|d| (OrderedFloat(d.current), d.time))
+    let (minc, mint) = data.iter().map(|d| (OrderedFloat(d.current), d.time))
 		.min_by(|(x,_),(y,_)| x.cmp(y)).unwrap();
-	let minc = minc.into_inner();
-	let minp = data.iter().position(|d| d.time == mint).unwrap();
+    let minc = minc.into_inner();
+    let minp = data.iter().position(|d| d.time == mint).unwrap();
 	
-	println!("max ct {:.1} ma {:.1} us {:.1} pt", maxc, maxt, maxp);
-	println!("min ct {:.1} ma {:.1} us {:.1} pt", minc, mint, minp);
+    println!("max ct {:.1} ma {:.1} us {:.1} pt", maxc, maxt, maxp);
+    println!("min ct {:.1} ma {:.1} us {:.1} pt", minc, mint, minp);
 
-	let t0 = (maxp+minp)/2;
-	let i1 = integ(&data,0,t0);
-	let i2 = integ(&data,t0,data.len()-1);
+    let t0 = (maxp+minp)/2;
+    let i1 = integ(&data, 0, t0);
+    let i2 = integ(&data, t0, data.len()-1);
 	
-	println!("i1 = {:.1} uJ",i1/1000.);
-	println!("i2 = {:.1} uJ",i2/1000.);
-    println!("COP = {:.1}%",f64::abs(100.*i2/i1));
+    println!("i1 = {:.1} uJ", i1/1000.);
+    println!("i2 = {:.1} uJ", i2/1000.);
+    println!("COP = {:.1}%", f64::abs(100.*i2/i1));
 }
 
     
